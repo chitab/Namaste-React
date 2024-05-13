e108ad9a0dc5f7d637c09e604fbe807fb067cfc9
Chitab Kumar <35214320+chitab@users.noreply.github.com>
2024-05-13 22:14:58 +0530

    episode 2

---
+++ Chapter 02 - Igniting Our App/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import  ReactDOM from "react-dom";
+const heading = React.createElement(
+  "div",
+  { id: "grandParent" },
+  React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+      "div",
+      { id: "child" },
+      [React.createElement("h1", { id: "heading" }, "React Hello World"),
+      React.createElement("h5", { id: "heading" }, "React Hello World")]
+    )
+  )
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
